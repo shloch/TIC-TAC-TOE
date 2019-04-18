c7626b8228027860791d81a40c1706420518fe8e
Oghenebrume50 <raphlbrume@gmail.com>
2019-04-18 15:25:00 +0100

    refactored codes playgame.rb added

---
+++ lib/playgame.rb
@@ -0,0 +1,38 @@
+class GamePlay
+  def initialize(player1, player2, game, messages)
+    @player1 = player1
+    @player2 = player2
+    @game = game
+    @messages = messages
+  end
+
+  def player_turn(player)
+    selected_box = player.select_box(@game, @messages)
+    @game.fillArray(player.play_letter, selected_box.to_i)
+    @game.showArray
+    player.update_play_record(selected_box)
+
+    @game.check_winner?(player)
+  end
+
+  def loop_play
+    play_count = 0
+    while play_count < 9
+      win = player_turn(@player1)
+      if win == true
+        @messages.announce_winner(@player1); break
+      end
+      play_count += 1
+
+      if play_count == 9
+        @messages.announce_winner; break
+      end
+
+      win = player_turn(@player2)
+      if win == true
+        @messages.announce_winner(@player2); break
+      end
+      play_count += 1
+    end
+  end
+end
