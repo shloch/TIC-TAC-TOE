52508ce27ba9a5655bcce4865aab0bd97eccd7d6
Oghenebrume50 <raphlbrume@gmail.com>
2019-04-19 13:54:33 +0100

    done with testing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,43 +7,43 @@ describe Game do
     @game = Game.new
   end
 
-  describe "#check_winner?" do
-    it "Should return false for an empty selection" do
+  describe '#check_winner?' do
+    it 'Should return false for an empty selection' do
       @player.instance_variable_set(:@play_record, [])
       expect(@game.check_winner?(@player)).to be_falsey
     end
 
-    it "Should return true with three matching plays" do
-      @player.instance_variable_set(:@play_record, ['1', '5', '7', '9'])
+    it 'Should return true with three matching plays' do
+      @player.instance_variable_set(:@play_record, %w(1 5 7 9))
       expect(@game.check_winner?(@player)).to be_truthy
     end
   end
 
-  describe "#fillArray" do
-    it "Should return an array with provide value" do
-      expect(@game.fill_array('X',5)).to eq('X')
+  describe '#fillArray' do
+    it 'Should return an array with provide value' do
+      expect(@game.fill_array('X', 5)).to eq('X')
     end
   end
 
-  describe "#numberBetween_1_9?" do
-    it "Should return true if number is between 1 an 9" do
-      expect(@game.numberBetween_1_9?(4)).to be_truthy
+  describe '#number_between_1_9?' do
+    it 'Should return true if number is between 1 an 9' do
+      expect(@game.number_between_1_9?(4)).to be_truthy
     end
 
-    it "Should return false if number is not between 1 an 9" do
-      expect(@game.numberBetween_1_9?(0)).to be_falsey
+    it 'Should return false if number is not between 1 an 9' do
+      expect(@game.number_between_1_9?(0)).to be_falsey
     end
   end
 
-  describe "#numberAlreadyChosen?" do
-    it "Should return true if number was chosen" do
-      @game.fill_array('X',5)
-      expect(@game.numberAlreadyChosen?(5)).to be_truthy
+  describe '#numberAlreadyChosen?' do
+    it 'Should return true if number was chosen' do
+      @game.fill_array('X', 5)
+      expect(@game.number_already_chosen?(5)).to be_truthy
     end
 
-    it "Should return false if number not chosen" do
-      @game.fill_array('X',5)
-      expect(@game.numberAlreadyChosen?(6)).to be_falsey
+    it 'Should return false if number not chosen' do
+      @game.fill_array('X', 5)
+      expect(@game.number_already_chosen?(6)).to be_falsey
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,20 +8,19 @@ describe Player do
   describe '#update_play_record' do
     it 'Should return an array' do
       expect(player.update_play_record('5')).to be_an_instance_of(Array)
-    end  
-   
+    end
+
     it 'Should equal an array with the selected box' do
-      expect(player.update_play_record('9')).to eq(['5', '9'])
+      expect(player.update_play_record('9')).to eq(%w(5 9))
     end
   end
 
   describe '#select_box' do
-    game = Game.new
-    message = GameMessage.new
-    player.stub!(:gets) { '5' }
-
-    it 'Should return the selected box' do
-      expect(player.select_box(game, message)).to eq('5')
+    it 'Should' do
+      game = Game.new
+      message = GameMessage.new
+      allow(player).to receive(:gets).and_return('6')
+      expect(player.select_box(game, message)).to eq('6')
     end
   end
 end
