5cfef0630b014fa3ea07358d1d7de6e589721cdd
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-04-19 11:57:42 +0100

    RSPEC continues adjustments

--- bin/launchgame.rb
+++ bin/launchgame.rb
@@ -20,7 +20,7 @@ name2 = gets.chomp
 player1 = Player.new(name1, 'X')
 player2 = Player.new(name2, 'O')
 
-new_game.showArray
+new_game.show_array
 play_game = GamePlay.new(player1, player2, new_game, game_message)
 play_game.loop_play
 game_message.game_over
